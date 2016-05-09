3dfe06e260486a4ab3ece5799fc602ec6252c4c7
adm <adm@adm-Komputer>
2016-05-09 16:30:35 +0200

    Merge origin/master
    Conflicts:
    	src/Neuron/Neuron.java

---
+++ src/allcreate/Settings.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package allcreate;
+
+
+public class Settings {
+    private String serverName;
+    private int portNr;
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public int getPortNr() {
+        return portNr;
+    }
+
+    public void setPortNr(int portNr) {
+        this.portNr = portNr;
+    }
+    
+    
+}
