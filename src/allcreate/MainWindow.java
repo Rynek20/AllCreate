415d603640aa7167262d7dcd5cb4a343032de788
adm <adm@adm-Komputer>
2016-05-09 14:11:10 +0200

    Merge origin/master
    Conflicts:
    	src/Neuron/Neuron.java

---
+++ src/allcreate/MainWindow.java
@@ -0,0 +1,17 @@
+
+package allcreate;
+
+/**
+ *
+ * @author adm
+ */
+public class MainWindow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
