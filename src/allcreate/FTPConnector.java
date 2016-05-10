bb17601bf406ca893d9c5d1e03bbf0a058a65868
adm <adm@adm-Komputer>
2016-05-10 10:22:31 +0200

    Merge origin/master
    Conflicts:
    	src/Neuron/Neuron.java

--- src/allcreate/FTPConnector.java
+++ src/allcreate/FTPConnector.java
@@ -1,39 +1,80 @@
-
 package allcreate;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
-
 public class FTPConnector {
-   private String server; 
-   private int port;
+
+    public enum Status {
+
+        CONNECTED, DISCONNECTED
+    }
+
+    private String server;
+    private int port;
     private String login;
     private String password;
     private final FTPClient ftpClient;
-    
-    public FTPConnector(){
+    private Status status;
+
+    public FTPConnector() {
         this.ftpClient = new FTPClient();
     }
-    
-    public boolean connect(String login, String password){
-        try{
-            ftpClient.connect(server,port);
+
+    FTPConnector(Settings actualSettings) {
+        this.ftpClient = new FTPClient();
+        this.server = actualSettings.getServerName();
+        this.port = actualSettings.getPortNr();
+        this.status = Status.DISCONNECTED;
+    }
+
+    public boolean connect(String login, String password) throws IOException {
+        try {
+            password = password.replaceAll(",", "");
+            password = password.replaceAll(" ", "");
+            ftpClient.connect(server, port);
             showServerReply();
-            int reply = ftpClient.getReply();
-            if(FTPReply.isPositiveCompletion(reply)){
-                return false;
-            }
             boolean loginReply = ftpClient.login(login, password);
             showServerReply();
-            return loginReply;
-        }catch(IOException e){
+            if (loginReply) {
+                status = Status.CONNECTED;
+                ftpClient.enterLocalPassiveMode();
+                ftpClient.changeWorkingDirectory("/kamilr");
+                System.out.println("Current directory is " + ftpClient.printWorkingDirectory());
+                FTPFile[] ftpFiles = ftpClient.listFiles();
+                if (ftpFiles != null && ftpFiles.length > 0) {
+                    for (FTPFile file : ftpFiles) {
+                        if (!file.isFile()) {
+                            continue;
+                        }
+                        System.out.println("File is " + file.getName());
+                    }
+                }
+                ftpClient.logout();
+                ftpClient.disconnect();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return true;
     }
-    
+
+    public boolean disconnect() {
+        try {
+            ftpClient.disconnect();
+        } catch (IOException ex) {
+            Logger.getLogger(FTPConnector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;
+    }
+
     private void showServerReply() {
         String[] replies = ftpClient.getReplyStrings();
         if (replies != null && replies.length > 0) {
